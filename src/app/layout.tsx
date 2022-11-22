ef7db41690e3c9af78fa667e01f98b6ee2542bd8
Alexis Barba <alexbarba.2278@gmail.com>
2022-11-21 23:23:45 -0700

    add root layout in appDir

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import "styles/globals.css";
+// import { Inter } from "@next/font/google";
+
+// const inter = Inter({
+//   weight: ["400", "700"],
+// }); className={inter.className}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>
+        <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+              Intercambio{" "}
+              <span className="text-[hsl(280,100%,70%)]">Navideño</span>
+            </h1>
+            {children}
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
