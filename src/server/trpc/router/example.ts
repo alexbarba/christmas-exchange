89d8a6e84c10cafa822416eed65aeac5deb3c55d
Alexis Barba <alexbarba.2278@gmail.com>
2022-11-24 11:36:58 -0700

    fix trpc router

--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -1,16 +1,9 @@
-import { z } from "zod";
+// import { z } from "zod";
 
 import { router, publicProcedure } from "../trpc";
 
 export const exampleRouter = router({
-  hello: publicProcedure
-    .input(z.object({ text: z.string().nullish() }).nullish())
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
-    }),
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+    return ctx.prisma.giftExchange.findMany();
   }),
 });
